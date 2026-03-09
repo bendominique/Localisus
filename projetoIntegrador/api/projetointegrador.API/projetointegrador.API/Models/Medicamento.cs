4a3b7c1a0281db0c48c897dcac3e72a3bb79970f
BENJAMIN DOMINIQUE MAKANGWA <benjamin.dmakangwa@senacsp.edu.br>
2026-03-09 17:35:55 -0300

    Envio da atividade

---
+++ projetoIntegrador/api/projetointegrador.API/projetointegrador.API/Models/Medicamento.cs
@@ -0,0 +1,11 @@
+﻿namespace projetointegrador.API.Models
+{
+    public class Medicamento
+    {
+        public int IdMedicamento { get; set; }
+        public string NomeMedicamento { get; set; }
+        public float Dosagem { get; set; }
+        public int Quantidade{ get; set; }
+        
+    }
+}
