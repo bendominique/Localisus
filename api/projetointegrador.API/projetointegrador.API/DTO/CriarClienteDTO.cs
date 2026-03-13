7ba98bfd4fee77681de5112a753954cf8283a8cb
BENJAMIN DOMINIQUE MAKANGWA <benjamin.dmakangwa@senacsp.edu.br>
2026-03-13 15:39:52 -0300

    Adicionando Controllers e Entidades para Medicamentos, Clientes e Endereço

---
+++ api/projetointegrador.API/projetointegrador.API/DTO/CriarClienteDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace projetointegrador.API.DTO
+{
+    public class CriarClienteDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+    }
+}
---
+++ api/projetointegrador.API/projetointegrador.API/DTO/CriarMedicamentoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace projetointegrador.API.DTO
+{
+    public class CriarMedicamentoDTO
+    {
+        public string NomeMedicamento { get; set; }
+        public float Dosagem { get; set; }
+        public int Quantidade { get; set; } 
+    }
+}
