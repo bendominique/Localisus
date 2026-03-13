e9c910e0576e3d84b04c8d770d43396b41eeae6d
BENJAMIN DOMINIQUE MAKANGWA <benjamin.dmakangwa@senacsp.edu.br>
2026-03-13 15:52:05 -0300

    Excluindo arquivos certos

--- apiUsuariosLocalisus/apiUsuariosLocalisus/apiUsuariosLocalisus/apiUsuariosLocalisus/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace apiUsuariosLocalisus.Models
-{
-    public class Login
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
-    }
-}
