fde773e3287fab4dbef13a982d5bdb851ea9ec48
bendominique <benbenjamim936@gmail.com>
2026-05-08 20:51:15 -0300

    Implementando salvamento em memória, impedindo que o usuário perca os seus dados num momento de F5

--- Frontend/Localisus/src/contexts/AuthContext.tsx
+++ Frontend/Localisus/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, ReactNode} from 'react';
+import { createContext, useState, useContext, ReactNode, useEffect} from 'react';
 import { authService } from '../services/authService';
 import { TipoUsuario, Usuario } from '../mocks/usuarioMock'
 
@@ -13,15 +13,33 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData> ({} as AuthContextData);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+
+    //é um hook onde o seu elemento interno é a primeira coisa que inicializa
+    //no momento em que a tela renderiza
+    useEffect(() => {
+        //realizando uma busca no nosso HD -> HardDisk, aquele que guarda todos os dados da memória do nosso PC
+        //procura a chave 
+        const usuarioSalvo = localStorage.getItem('@localisus:usuario')
+
+        if(usuarioSalvo) { //caso encontre algum usuário em memória, vai enviar em JSON.parse, transformando ele em objeto
+            setUsuario(JSON.parse(usuarioSalvo))
+
+        }
+    }, []) //permite que inicialize apenas no momento em que a tela renderiza
+
     const [usuario, setUsuario] = useState<Usuario | null>(null)
 
     const login = async (cpf: string, senha: string) => {
-        const userEncontrado = await authService.login(cpf, senha)
-        setUsuario(userEncontrado)
+        const usuarioEncontrado = await authService.login(cpf, senha)
+        setUsuario(usuarioEncontrado)
+
+        localStorage.setItem('@localisus:usuario', JSON.stringify(usuarioEncontrado))
     }
 
 const logout = () => {
     setUsuario(null)
+    //tira da memória
+    localStorage.removeItem('@localisus:usuario')
 }
 
 const register = async (nome: string, cpf: string, email: string, senha: string, tipo: TipoUsuario, hospitalId: number | null) => {
@@ -30,6 +48,7 @@ const register = async (nome: string, cpf: string, email: string, senha: string,
     const novoUsuario = await authService.register(nome, cpf, email, senha, tipo, hospitalId)
      setUsuario(novoUsuario)
 
+     localStorage.setItem('@localisus:usuario', JSON.stringify(novoUsuario))
     } catch (error: any) {
         console.error("Erro ao registrar novo usuário", error.message)
     }
