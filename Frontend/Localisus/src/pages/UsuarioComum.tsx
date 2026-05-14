aa36b5b850ce1daeb1599e63ba7b4ceefa73d0bd
BENJAMIN DOMINIQUE MAKANGWA <benjamin.dmakangwa@senacsp.edu.br>
2026-05-14 17:09:24 -0300

    Implementando nova otimização para home usuário

--- Frontend/Localisus/src/pages/UsuarioComum.tsx
+++ Frontend/Localisus/src/pages/UsuarioComum.tsx
@@ -1,12 +1,17 @@
+import "./UsuarioComum.css"
 import Navbar from "../components/Navbar"
-import { Cards } from "../components/Cards"
-import { CardData } from "../components/Cards"
 import { ComponenteCard } from "../components/Cards"
-import { BarraPesquisa } from "../components/BarraPesquisa"
-import { MapaLocalisus } from "../components/MapaLocalisus"
 
-const elementosCard: CardData[] = [
-    { id: 1, titulo: "dads", descricao: "" }
+const cardsCentro = [
+    { titulo: "Histórico de retiradas", descricao: "Veja com qual frequência você tem retirado seus medicamentos" },
+    { titulo: "Lembretes", descricao: "Verifique se possui algum medicamento para tomar hoje" },
+    { titulo: "Como funciona", descricao: "Entenda como utilizar o site, encontrar seu medicamento e muito mais" },
+    { titulo: "Meus medicamentos", descricao: "Acompanhe seus e remédios e receba lembretes" }
+]
+
+const cardsDireita = [
+    { titulo: "Locali", descricao: "Assistente virtual do localisus" },
+    { titulo: "Últimas Notícias", descricao: "Alera: Surto de hantavírus" }
 ]
 
 
@@ -15,20 +20,44 @@ export const UsuarioComum = () => {
     return (
         <>
             <header>
-
+                    <div className="cabecalho-nav">
+                        <label className="container-login">
+                            <h4>Nome Usuário</h4>
+                            <p>Tipo de Usuário</p>
+                        </label>
+                    </div>
             </header>
             <main>
-                <h1>Olá </h1>
-                <div className="componentes-centro">
-                    <ComponenteCard titulo="Meus medicamentos" descricao="Acompanhe seus e remédios e receba lembretes" />
-                    <ComponenteCard titulo="Postos próximos" descricao="Veja unidades de saúde perto de você" />
-                    <ComponenteCard titulo="Disponibilidade" descricao="Confira a disponibilidade em tempo real" />
-                    <ComponenteCard titulo="Como funciona" descricao="Entenda como encontrar seu medicamento" />
-                </div>
-                <div className="componentes-lateral-direita">
-                    <ComponenteCard titulo="Locali" descricao="Assistente virtual do localisus" />
-                    <ComponenteCard titulo="Lembretes" descricao="" />
-                    <ComponenteCard titulo="Seu histórico" descricao="histórico medicamentos"/>
+                <div className="conteudo-pagina">
+
+                    <h1 id="saudacao-user">Olá, Nome</h1>
+                    <h4 id="sugestao-user">Como podemos ajudar você hoje?</h4>
+                    <div className="cartao-busca">
+                        <h2> Encontre seu medicamento </h2>
+                        <h4> Pesquise e veja onde encontrar gratuitamente por uma farmácia ou pelo SUS</h4>
+                        <p>Mais buscados: </p>
+                    </div>
+                    <section className="componentes-pesquisa">
+                        {
+                            cardsCentro.map(cc =>
+                                <article className="cards-usuario-comum-centro">
+                                    <ComponenteCard titulo={cc.titulo} descricao={cc.descricao} />
+                                </article>
+                            )
+                        }
+                    </section>
+                    <section className="componentes-lateral-direita">
+                        {
+                            cardsDireita.map(cd =>
+                                <article className="elementos-card-usuario-comum-direita">
+                                    <ComponenteCard titulo={cd.titulo} descricao={cd.descricao} />
+                                </article>
+                            )
+                        }
+                    </section>
+                    <div className="mapa">
+
+                    </div>
                 </div>
             </main>
         </>
