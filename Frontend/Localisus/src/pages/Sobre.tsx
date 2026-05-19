63a075942c61c3e05636da3525a8ba5ed952d119
bendominique <benbenjamim936@gmail.com>
2026-05-19 20:50:18 -0300

    começando estilização do Sobre

--- Frontend/Localisus/src/pages/Sobre.tsx
+++ Frontend/Localisus/src/pages/Sobre.tsx
@@ -12,12 +12,12 @@ export const Sobre = () => {
         <>
 
             <header>
-                <Navbar />
+                {/* <Navbar /> */}
             </header>
             <main>
                 <section className="oq-localisus">
-                    <h4>O QUE É O LOCALISUS </h4>
-                    <h2>Tecnologia que aproxima você do que realmente importa:<span>sua saúde</span>.</h2>
+                    <h4>O QUE É O LOCALISUS? </h4>
+                    <h2>Tecnologia que aproxima você do que realmente importa: <span>sua saúde</span>.</h2>
                     <p> O Localisus é uma plataforma inteligente que conecta você aos medicamentos, postos de saúdes, farmácias e informações para que você cuide melhor de si e de quem ama.</p>
                 </section>
                 <section className="resposta-localisus">
