ad9335849eeab5b80e4a50312518af47115e46df
BENJAMIN DOMINIQUE MAKANGWA <benjamin.dmakangwa@senacsp.edu.br>
2026-06-17 16:46:06 -0300

    Limpando os arquivos desnecess[aerops

--- projetointegrador.API/projetointegrador.API/DTO/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace projetointegrador.API.DTO
-{
-    public class LoginDTO
-    {
-        public string CPF { get; set; } = string.Empty; 
-        public string Senha { get; set; } = string.Empty;
-    }
-}
--- projetointegrador.API/projetointegrador.API/Enum/TipoEndereco.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace projetointegrador.API.Enum
-{
-    public enum TipoEndereco
-    {
-        Residencial = 1,
-        Comercial = 2,
-    }
-}
